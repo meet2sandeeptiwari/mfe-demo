442c054a466f52cc33018c764cb739be201e5b7c
sandeeptiwari01 <meet2sandeeptiwari@gmail.com>
2026-08-16 18:36:45 +0530

    bugg fix

--- projects/shell/src/app/layout/header/header.ts
+++ projects/shell/src/app/layout/header/header.ts
@@ -1,9 +1,10 @@
 import { Component, inject } from '@angular/core';
 import { ShellAuthService } from '../../services/shell-auth-service';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './header.html',
   styleUrl: './header.scss',
 })
